a010cd8d16352f670a86d8516cb1d0df9c1d80f1
Shivam Pathak <113749553+shivampathak2812@users.noreply.github.com>
2025-12-10 00:27:34 +0530

    Update intermediate_SQL.sql

--- intermediate_SQL.sql
+++ intermediate_SQL.sql
@@ -1,3 +1,24 @@
+Intermediate Level (16–35)
+Show employees from HR or Finance department.
+Show employees whose salary is between 40,000 and 70,000.
+Show employees whose first name contains the letter ‘h’.
+Show employees whose department is not IT.
+Show employees whose status is Inactive.
+Count employees in each department.
+Find the average salary of IT department.
+Show the highest salary in the employees table.
+Show the lowest salary in the employees table.
+Show employees who joined in the last 2 years.
+Show employees whose phone number starts with ‘98’.
+Show all employees except those from Sales department.
+Show total salary expenditure of all employees.
+Show employees grouped by designation.
+Show employees whose email contains “gmail”.
+Show employees who have the same department and status = Active.
+Show top 5 highest paid employees.
+Show first 10 employees based on emp_id.
+Show employees who joined in February (any year).
+Show employees with salary ending in “000”.
 select * from employees where department in ("HR","Finance");
 select * from employees where salary between 40000 and 70000;
 select * from employees where first_name like "%H%";
